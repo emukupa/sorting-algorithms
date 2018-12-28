a5fb4509112c030739c5f10ed952e0d003b99503
Edward Manda <lilkups@yahoo.com>
2018-12-28 10:07:13 -0600

    added an optimization

--- bubblesort/java/BubbleSort.java
+++ bubblesort/java/BubbleSort.java
@@ -1,9 +1,10 @@
 public class BubbleSort {
-
+  // naive, only useful if you want to sort one or few elements
+  // slow, O(n) passes by O(n) times => giving us the complexity of O(n^2)
   public static void bubble(int[] arr) {
     // assume its not sorted
     boolean isSorted = false;
-
+    int lastSorted = arr.length - 1;
     // start walking through over and over until its sorted
     while (!isSorted) {
       // every start, we assume its sorted
@@ -12,12 +13,13 @@ public class BubbleSort {
       // we will use a for loop comparing the current element with the next one
       // to prevent out of bound error, we end at one element less the length of the
       // array
-      for (int i = 0; i < arr.length - 1; i++) {
+      for (int i = 0; i < lastSorted; i++) {
         if (arr[i] > arr[i + 1]) {
           swap(arr, i, i + 1);
           isSorted = false; // we know the array is not sorted
         }
       }
+      lastSorted--; // optimization
     }
   }
 
